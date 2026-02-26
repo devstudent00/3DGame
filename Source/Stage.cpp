7cf5b66ed617c595aa5399349b0571217550d23f
250190 <250190@jc-21.jp>
2026-02-26 13:44:42 +0900

    アイテムスロットを追加しました

--- Source/Stage.cpp
+++ Source/Stage.cpp
@@ -1,6 +1,7 @@
 #include "Stage.h"
 #include <cassert>
 #include "ChatUI.h"
+#include "Screen.h"
 
 Stage::Stage()
 {
@@ -22,6 +23,22 @@ void Stage::Update()
 void Stage::Draw()
 {
 	MV1DrawModel(hModel);
+
+	// アイテムUI
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 150);
+	int sizeX = 500, sizeY = 150;
+	int leftX = Screen::WIDTH / 2 - (sizeX / 2), leftY = Screen::HEIGHT - (sizeY / 2);
+	int rightX = Screen::WIDTH / 2 + (sizeX / 2), rightY = Screen::HEIGHT;
+	DrawBox(leftX, leftY, rightX, rightY, GetColor(0, 0, 0), true);
+	
+	//アイテムの仕切りを描画
+	int itemNum = 5;
+	for (int i = 0; i < itemNum + 1; i++) {
+		int lineX = leftX + (sizeX / itemNum) * i;
+		DrawLine(lineX, leftY, lineX, rightY, GetColor(255, 255, 255));
+		DrawLine(leftX, leftY, rightX, leftY, GetColor(255, 255, 255));
+	}
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 }
 
 bool Stage::CollideRay(VECTOR3 start, VECTOR3 end, VECTOR3* hit) {
